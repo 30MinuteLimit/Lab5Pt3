7fa00a3e10ede3c0492edd08f8720253db78b6e8
unknown <nowonlinetoo@gmail.com>
2023-10-26 11:11:13 -0700

    added lab5Ptstart.c

---
+++ lab5Ptstart.c
@@ -0,0 +1,51 @@
+/* Lab 5 P3: starter code to complete*/
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char input[256];
+    int letterCount[26] = {0}; // Array to store the count of each letter initialized to zeros
+
+    printf("Enter text strings (press Enter on an empty line to exit):\n");
+
+    while (1)
+    {
+
+        fgets(input, sizeof(input), stdin); // get the input from the user via keyboard
+
+        // Check for an empty line to exit the loop
+        if (strlen(input) == 1 && input[0] == '\n')
+        {
+            break;
+        }
+        // Your Code should be implemented here
+        // Iterate through the characters in the input
+
+        // Check if the character is a letter (A-Z or a-z)
+        for (int letters = 65; letters <= 90; letters++ ){
+        int value = 0;
+        printf("%c: " , letters);
+
+        for (int i = 0; i < strlen(input); i++){
+       
+        
+        // Increment the corresponding count in the array
+        
+
+            if(input[i] == letters || input [i] == letters+32){
+               value++; 
+            }
+
+        }
+        printf("%d ", value);
+        }
+        
+        
+break;
+    }
+    // More of your code here to
+    // Display the letter counts
+
+    return 0;
+}
